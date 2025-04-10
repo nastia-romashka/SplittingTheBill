9f8d2a04661266762657938061ddfc480bc05cb9
EmilBBbbbbbb <emilianevgenevishborisov@gmail.com>
2025-04-10 14:12:24 +0300

    Picture part

--- ORC.py
+++ ORC.py
@@ -1,6 +1,67 @@
 import easyocr
 from collections import defaultdict
 import cv2
+from loguru import logger
+import sys
+import re
+import numpy as np
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+
+
+def search_price_coord(result: list, tolerance: int)->list:
+
+    numbs = []
+    for (bbox, text, prob) in result:
+        if re.fullmatch(r"^\d+[.,]\d+$", text):
+            numbs.append([text,bbox])
+
+    max_x = -1
+    for text, bbox in numbs:
+        if max_x < bbox[1][0]:
+            max_x = bbox[1][0]
+
+    price = []
+    for text, bbox in numbs:
+        if (max_x - bbox[1][0])<=tolerance:
+            price.append([text,bbox])
+
+    return price
+
+def picture_part(price: list, path: str)->None:
+    image = cv2.imread(path)
+    height, width = image.shape[:2]
+
+    count=0
+    for i in range(len(price)-1):
+        # Извлекаем Y-координаты (верхнюю и нижнюю границы)
+        y_top = price[i][1][1][1]  # Верхняя граница (первая точка, Y-координата)
+        y_bottom = price[i+1][1][1][1]  # Нижняя граница (третья точка, Y-координата)
+
+        # Вырезаем полосу по всей ширине изображения
+        vertical_stripe = image[y_top:y_bottom, 0:width]  # 0:width - вся ширина
+
+        # Сохраняем результат
+        cv2.imwrite(f'{i}.jpg', vertical_stripe)
+        logger.debug(f"Вертикальная полоса сохранена.")
+        count+=1
+
+def join_name_count_price(result: list) ->list[str]:
+
+    name = ''
+    for _, text, _ in result:
+        if re.fullmatch(r"^\D+$", text):
+            name+=text+' '
+
+    join = [name]
+
+    for _, text, _ in result:
+        if re.fullmatch(r"^\d+[.,]\d+$", text):
+            join.append(text)
+
+    return join
+
 
 def img_show(img_path: str, result):
     img = cv2.imread(img_path)
@@ -13,15 +74,25 @@ def img_show(img_path: str, result):
                       thickness=2)
     cv2.imshow("", img)
     cv2.waitKey(0)
+
 def OCR(img_path: str):
+
+    # Погрешность для группировки строк (подбирается экспериментально)
+    TOLERANCE = 20  # пикселей
+
+
     # Инициализация EasyOCR
     reader = easyocr.Reader(['ru', 'en'])
 
     # Распознавание текста с изображения чека
     result = reader.readtext(img_path)
 
-    # Погрешность для группировки строк (подбирается экспериментально)
-    TOLERANCE = 10  # пикселей
+    logger.info(result)
+    logger.info(search_price_coord(result, TOLERANCE))
+
+    picture_part(search_price_coord(result, TOLERANCE), img_path)
+
+
 
     # Группировка текста по строкам с учетом погрешности
     lines = defaultdict(list)
@@ -59,5 +130,11 @@ def OCR(img_path: str):
 
     img_show(img_path, result)
 
+    return result
+
 if __name__ == '__main__':
-    OCR('images/img.jpg')
+    #OCR('images/img.jpg')
+
+    res = OCR('1.jpg')
+
+    join_name_count_price(res)
